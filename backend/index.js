4d1a109571a69aa78d8194e24106a95b6e8f148b
eduardojz <ezamorav@fwdcostarica.com>
2023-07-27 13:36:50 -0600

    new Update

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,51 @@
 /*---------------Funciones--------------------*/
 function actualizarContador() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    let contador = 0
+    let contador = 0;
     checkboxes.forEach(checkbox => {
         if (checkbox.checked) {
             contador++;
         }
     });
-    document.getElementById('contador').innerText = contador
+    document.getElementById('contador').innerText = contador;
 }
 /*------------------Crear botones----------------*/
 function guardar() {
-    let task = document.getElementById('task')
-    if (verificar(task.value.toLowerCase())==false){
-        return false
+    const container=document.getElementById('contenedorTareas');
+    let task = document.getElementById('task');
+    if (!verificar(task.value.toLowerCase())==false){
+        alert("La tarea ya existe")
+        return false;
     }
-    task.textContent=''
-    const table = document.getElementById('tbodyTask')
+    container.appendChild(createTable(container));
+    alertEmpty();
+}
+function createTable(container){
+    let table=container.querySelector('table');
+    if (table===null) {
+        table = document.createElement('table');
+        table.classList='table'
+    }
+    const tbody = document.createElement('tbody');
     const tr = document.createElement('tr');
-    for (let i = 0; i < 3 ; i++) {
-        const td = document.createElement('td');
-        td.classList.add("col-4");
-        if (i===0) {
-            td.appendChild(check())
-        }
-        if (i===1) {
-            td.textContent = task.value.toLowerCase();
-            td.classList.add("parrafo");
-        }
-        if(i===2){
-            td.appendChild(eliminar())
-        }
-        tr.appendChild(td)
-        
-    }
-    table.appendChild(tr)
+    const taskColumn = document.createElement('td');
+    //
+    const checkTd=document.createElement('td');
+    const deleteTd=document.createElement('td');
+    //
+    taskColumn.classList='parrafo'
+    taskColumn.textContent=task.value;
+    checkTd.appendChild(check())
+    deleteTd.appendChild(eliminar())
+    //
+    task.value='';
+    tr.appendChild(checkTd);
+    tr.appendChild(taskColumn);
+    tr.appendChild(deleteTd);
+    tbody.appendChild(tr)
+    //here 16:01
+    table.appendChild(tbody);
+    return table;
 }
 function check() {
     const check = document.createElement('input')
@@ -43,19 +54,20 @@ function check() {
     check.addEventListener('click', () => {
         actualizarContador()
     })
-    return check
+    return check;
 }
 //
 function eliminar() {
     const deletebtn = document.createElement('button')
     const img=document.createElement('img')
-    img.src="../img/borrar.png"
-    img.className="imgDelete"
-    deletebtn.appendChild(img)
+    img.src="../img/borrar.png";
+    img.className="imgDelete";
+    deletebtn.appendChild(img);
     deletebtn.className = 'btn-delete'
     deletebtn.addEventListener('click', (e) => {
         e.target.closest('tr').remove()
         actualizarContador()
+        alertEmpty()
     })
     return deletebtn
 }
@@ -65,14 +77,29 @@ document.querySelector('form').
         e.preventDefault()
         guardar()
     })
-function verificar(task){
-        const nodes=document.querySelectorAll(".parrafo")
-        if(nodes.length>0){
-            for (let i=0;nodes.length>0>i;i++){
-                if (nodes[i].textContent==task) {
-                    alert("Ya existe la tarea")
-                    return false;
-                }
-            }
+function verificar(task) {
+    const nodes=document.querySelectorAll(".parrafo")
+    const condition=(e)=> e.textContent===task;
+    return Array.from(nodes).some(condition);
+}
+document.addEventListener('DOMContentLoaded',alertEmpty)
+function alertEmpty(){
+    const container=document.getElementById('contenedorTareas');
+    const tbody=document.querySelector('tbody');
+    if (tbody===null || tbody.childElementCount<=0) { 
+        const warning=document.createElement('div'); 
+        warning.classList='alert alert-warning';
+        warning.role='alert';
+        warning.id='alertID';
+        warning.textContent='You dont have tasks';
+        container.append(warning)
+        if (tbody!==null) {            
+            tbody.parentNode.remove()
         }
+    }
+    let divAlert=document.getElementById('alertID')
+    
+    if(divAlert!==null && tbody!==null && tbody.childElementCount>0){
+        divAlert.remove()
+    }
 }
